5e32d166ba7791fef73d562eac956ff2c44b5e46
Afrin Dange <dangeafrin@gmail.com>
2025-08-14 21:06:52 +0530

    update

--- src/data/updates.js
+++ src/data/updates.js
@@ -1,6 +1,16 @@
 // src/data/updates.js
 export const updates = [
   {
+    date: "August 2025",
+    icon: "book",
+    text: "Our work on Instance-Level Adaptation of TSFMs has been accepted at CIKM 2025",
+  },
+  {
+    date: "July 2025",
+    icon: "map-pin",
+    text: "Attended LLM Summer School at Yerevan, Armenia",
+  },
+  {
     date: "June 2025",
     icon: "graduation-cap",
     text: "Completed my MS Thesis Defense",
@@ -11,37 +21,37 @@ export const updates = [
     text: "Reviewer for ICML 2025 Workshop on Foundation Models for Structured Data",
   },
   {
-    date: "Jan 2025",
+    date: "January 2025",
     icon: "map-pin",
-    text: "Attended Google DeepMind Research Symposium at Bangalore",
+    text: "Attended Google DeepMind Research Symposium at Bangalore, India",
   },
   {
-    date: "Jan 2025",
+    date: "January 2025",
     icon: "map-pin",
-    text: "Attended RL Workshop at IISc Bangalore",
+    text: "Attended RL Workshop at IISc Bangalore, India",
   },
   {
-    date: "Jan 2025",
+    date: "January 2025",
     icon: "book-open",
     text: "Teaching Assistant for CS726: Advanced Machine Learning",
   },
   {
-    date: "Nov 2024",
+    date: "November 2024",
     icon: "pen-line",
     text: "Reviewer for ICLR 2025",
   },
   {
-    date: "Aug 2024",
+    date: "August 2024",
     icon: "mic",
     text: "Talk: ''Conditional Generation Using GANs and Diffusion Models''",
   },
   {
     date: "July 2024",
     icon: "book",
-    text: "Paper published in ICML 2024 TF2M Workshop",
+    text: "Our work on ICL-enabled transformer designs for TSFMs has been accepted at ICML 2024 TF2M Workshop",
   },
   {
-    date: "Feb 2024",
+    date: "February 2024",
     icon: "mic",
     text: "Talk: ''Generative AI for Vision''",
   },
